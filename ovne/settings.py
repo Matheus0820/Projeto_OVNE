858f8586d60579f690286834f522b70366c83c39
Matheus0820 <matheus.ramos.703@ufrn.edu.br.com>
2024-02-04 23:38:17 -0300

    fix: static 3

--- ovne/settings.py
+++ ovne/settings.py
@@ -125,7 +125,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'staticfiles'),
+    os.path.join(BASE_DIR, 'controller/static'),
+    os.path.join(BASE_DIR, 'observatorio/static')
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
