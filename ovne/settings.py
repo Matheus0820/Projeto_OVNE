40ff3fe7c93efea7e37da5d3bd4afb237dfeaf55
Matheus0820 <matheus.ramos.703@ufrn.edu.br.com>
2024-02-04 22:08:50 -0300

    feat: add static path apps

--- ovne/settings.py
+++ ovne/settings.py
@@ -124,6 +124,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'controller/static'),
+    os.path.join(BASE_DIR, 'observatorio/static')
+]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
