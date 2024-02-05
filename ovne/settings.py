70db5976f773434b774f628e9e62d557c3f704ca
Matheus0820 <matheus.ramos.703@ufrn.edu.br.com>
2024-02-04 22:59:51 -0300

    fix: add static path apps

--- ovne/settings.py
+++ ovne/settings.py
@@ -125,6 +125,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
+    os.parent.join(BASE_DIR, 'staticfiles'),
     os.path.join(BASE_DIR, 'controller/static'),
     os.path.join(BASE_DIR, 'observatorio/static')
 ]
